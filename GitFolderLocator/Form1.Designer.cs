e962d14c8e43c588996f62ca532ccb89b497a2dc
shehan <shehan.peruma@gmail.com>
2018-01-05 02:49:18 +0000

    inital code commit

---
+++ GitFolderLocator/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace GitFolderLocator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(12, 58);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(454, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(12, 30);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(454, 20);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Root Folder Path:";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 89);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLog.Size = new System.Drawing.Size(454, 345);
+            this.textBoxLog.TabIndex = 3;
+            this.textBoxLog.Text = "Log:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 446);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Git Folder Locator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLog;
+    }
+}
+
---
+++ GitFolderLocator/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GitFolderLocator
+{
+    public partial class Form1 : Form
+    {
+        String rootDirectory;
+        StreamWriter outpuFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DoWork()
+        {
+            UpdateWorkStatusLog($"Started!");
+            outpuFile.WriteLine($"DirectoryPath");
+            outpuFile.Flush();
+
+            var directories = Directory.GetDirectories(rootDirectory);
+            using (outpuFile)
+            {
+                foreach (var dir in directories)
+                {
+                    var directoyList = Directory.EnumerateDirectories(dir, ".git", SearchOption.AllDirectories);
+
+                    foreach (var item in directoyList)
+                    {
+                        UpdateWorkStatusLog($"Processing: {item}");
+                        outpuFile.WriteLine($"\"{item}\"");
+                        outpuFile.Flush();
+                    }
+                }
+            }
+
+            outpuFile.Close();
+            UpdateWorkStatusLog($"Completed!");
+            MessageBox.Show("Done!");
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBoxPath.Text))
+            {
+                MessageBox.Show("Direcorty does not exist!");
+                return;
+            }
+            rootDirectory = textBoxPath.Text;
+            var outpuFileName = $"Output_{DateTime.Now.Ticks}.csv";
+            outpuFile = File.CreateText(outpuFileName);
+            UpdateWorkStatusLog($"Output File: {new FileInfo(outpuFileName).FullName}");
+
+            textBoxPath.Enabled = false;
+            buttonStart.Enabled = false;
+            Thread t = new Thread(DoWork);
+            t.Start();
+        }
+
+        private void UpdateWorkStatusLog(string text)
+        {
+            if (this.textBoxLog.InvokeRequired)
+            {
+                UpdateLogCallback callback = new UpdateLogCallback(UpdateWorkStatusLog);
+                this.Invoke(callback, new object[] { text });
+            }
+            else
+            {
+                textBoxLog.Focus();
+                textBoxLog.AppendText($"{Environment.NewLine}{DateTime.Now.ToString()} - {text}");
+            }
+        }
+
+        delegate void UpdateLogCallback(string text);
+    }
+}
